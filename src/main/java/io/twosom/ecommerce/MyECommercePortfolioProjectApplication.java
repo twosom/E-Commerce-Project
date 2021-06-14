08b517f955d2c317a340327544e7cf02becc0ba3
desirel <two_somang@icloud.com>
2021-06-14 11:45:00 +0900

    feat: initialize

---
+++ src/main/java/io/twosom/ecommerce/MyECommercePortfolioProjectApplication.java
@@ -0,0 +1,13 @@
+package io.twosom.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyECommercePortfolioProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyECommercePortfolioProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/twosom/ecommerce/MyECommercePortfolioProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.twosom.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyECommercePortfolioProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
