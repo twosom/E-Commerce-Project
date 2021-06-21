0f09ea27cd892d62a3803ca032ca1b6bcacf01d4
desirel <two_somang@icloud.com>
2021-06-21 15:13:29 +0900

    feat: prod yml 추가

--- src/main/java/io/twosom/ecommerce/mail/HtmlEmailService.java
+++ src/main/java/io/twosom/ecommerce/mail/HtmlEmailService.java
@@ -11,7 +11,7 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 @Slf4j
-@Profile("dev")
+@Profile(value = {"dev", "prod"})
 @Component
 @RequiredArgsConstructor
 public class HtmlEmailService implements EmailService {
